56105070cbd1ecd60fe5f6e09851471b8f58b733
yuanbaowen521 <yuanbaowen521@126.com>
2015-08-18 18:19:26 +0800

    new

---
+++ s-plot.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#set -x
+
+filename=`basename $0`
+
+usage()
+{
+cat <<EOF
+${txtcyn}
+Usage:
+
+${filename} options${txtrst}
+
+${bldblu}Function${txtrst}:
+
+This software is designed to simplify the process of plotting,
+allowing easy and flexible research. 
+
+Currently, the following types of plot are supported.
+
+#### Bars
+${filename} barPlot
+${filename} horizontalBar
+${filename} multiBarNew
+${filename} colorBar
+
+#### Lines
+${filename} lines
+
+#### Dots
+${filename} scatterplot
+${filename} scatterplotColor
+${filename} scatterplotContour
+${filename} scatterplotLotsData
+${filename} scatterplotMatrix
+${filename} scatterplotDoubleVariable
+${filename} contourPlot
+
+#### Distribution
+${filename} areaplot
+${filename} boxplot
+${filename} densityPlot
+${filename} densityHistPlot
+${filename} histogram
+
+#### Cluster
+${filename} hcluster
+${filename} hclust (depleted)
+
+#### Heatmap
+${filename} heatmapS
+${filename} heatmapM
+${filename} heatmap.2
+
+#### Others
+${filename} volcano
+${filename} vennDiagram
+
+EOF
+}
+
+if test $# -lt 1; then
+	usage
+	exit 1
+fi
+
+program="sp_"$1".sh"
+type ${program} >/dev/null 2>&1
+error=$?
+if test $error != 0; then
+	usage
+	echo "**Please check the program name input**"
+	exit 1
+else
+	shift
+	${program} "$@"
+fi
+
